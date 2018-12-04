db719538644e487a35dd2fd9bec28b906980856f
Joshy Orndorff <admin@joshorndorff.com>
2018-12-04 18:59:36 -0500

    Solved 3.2 in haskell

--- day3/hs/solution.hs
+++ day3/hs/solution.hs
@@ -1,40 +1,57 @@
 import System.IO
-import Data.List.Split
-import Data.List
+import qualified Data.Set as Set
 import qualified Data.Map as Map
+import Data.List
+import Data.List.Split
 
-type Rectangle = (Int, Int, Int, Int) -- (x, y, w, h)
-type Coordinate = (Int, Int) -- (x, y)
+type Claim = (Int, Int, Int, Int, Int)
+type Coordinate = (Int, Int)
 
-listToRect :: [Int] -> Rectangle
-listToRect [_, x, y, w, h] = (x, y, w, h)
-listToRect _ = (10, 20, 30, 40)
 
--- parse a line into a rectangle
-parseRect :: String -> Rectangle
-parseRect =
-  listToRect . map read . filter (/="") . splitOneOf "#@,:x "
+parseClaim :: String -> Claim
+parseClaim =
+  let
+    listToClaim :: [Int] -> Claim
+    listToClaim [i, x, y, w, h] = (i, x, y, w, h)
+    listToClaim _ = (0, 10, 20, 30, 40) -- inexhaustive...
+  in
+  listToClaim . map read . filter (/="") . splitOneOf "#@,:x "
 
--- Calculate list of Coords contained in rectangle
-rectToCoords :: Rectangle -> [Coordinate]
-rectToCoords (x, y, w, h) =
+-- Calculate list of Coords contained in a claim
+claimToCoords :: Claim -> [Coordinate]
+claimToCoords (_, x, y, w, h) =
   [(x', y') | x' <- [x+1..x+w] , y' <- [y+1..y+h]]
 
-flatten = intercalate []
 
-main = do
-  handle <- openFile "../input.txt" ReadMode
-  contents <- hGetContents handle
+
+overlapping :: [Claim] -> Set.Set Coordinate
+overlapping claims =
   let
-    rects = map parseRect (lines contents)
-    coordList = flatten (map rectToCoords rects)
+    flatten = intercalate []
+    coordList = flatten (map claimToCoords claims)
     weightedCoordList = map (\c -> (c, 1)) coordList
     coordCounts = Map.fromListWith (+) weightedCoordList
-    part1 = Map.size $ (Map.filter $ (>1)) coordCounts
-  print "rects:"
-  print rects
-  print "coordList:"
-  print coordList
+  in
+    Map.keysSet $ (Map.filter (>1)) coordCounts
+
+
+
+noneInCommon :: Set.Set Coordinate -> Set.Set Coordinate -> Bool
+noneInCommon a b = (==0) . Set.size $ Set.intersection a b
+
+
+
+main = do
+  handle <- openFile "../example.txt" ReadMode
+  contents <- hGetContents handle
+  let
+    claims = map parseClaim (lines contents)
+    overlappingCoords = overlapping claims
+    coordsByClaim = Map.fromList . map (\c -> (c, Set.fromList $ claimToCoords c)) $ claims
+    (goodClaim, _) = head . Map.toList . Map.filter (noneInCommon overlappingCoords) $ coordsByClaim
+    (goodId, _, _, _, _) = goodClaim
   print "part 1:"
-  print part1
+  print $ Set.size overlappingCoords
+  print "part 2:"
+  print goodId
   hClose handle
