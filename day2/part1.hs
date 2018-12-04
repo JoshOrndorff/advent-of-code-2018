1d1bc51cd98ff11a4000a748cc19b85239917904
Joshy Orndorff <admin@joshorndorff.com>
2018-12-03 21:46:12 -0500

    Solved day 2 part 1 in haskell.

--- day2/part1.hs
+++ day2/part1.hs
@@ -1,13 +1,34 @@
 import qualified Data.Map as Map
+import System.IO
 
---TODO I think reduce (or maybe a fold) with the map as the accumulator would work better
 -- Frequency analysis on a single string
-freqAnal :: Data.Map String Int -> String -> Data.Map String Int
-freqAnal soFar "" = soFar
-freqAnal soFar c:cs = freqAnal (soFar.addWith(c, (+)) cs)
+freqAnal ::  String -> Map.Map Char Int
+freqAnal s = Map.fromListWith (\x y -> x + y) (map (\x -> (x, 1)) s)
 
--- Checksum for a single string
-checksum :: String -> Int
-checksum s =
-  let freqs = freqAnal Data.Map s
+
+doubsTrips :: String -> (Bool, Bool)
+doubsTrips s =
+  let freqList = Map.toList $ freqAnal s
+      reducer (_, tripsYet) (_, 2) = (True, tripsYet)
+      reducer (doubsYet, _) (_, 3) = (doubsYet, True)
+      reducer (doubsYet, tripsYet) (_, _) = (doubsYet, tripsYet)
   in
+    foldl (reducer) (False, False) (freqList)
+
+countDoubsTrips :: [String] -> (Int, Int)
+countDoubsTrips xs =
+  let reducer :: (Int, Int) -> (Bool, Bool) -> (Int, Int)
+      reducer (doubs, trips) (True, True) = (doubs + 1, trips + 1)
+      reducer (doubs, trips) (True, False) = (doubs + 1, trips)
+      reducer (doubs, trips) (False, True) = (doubs, trips + 1)
+      reducer (doubs, trips) (False, False) = (doubs, trips)
+  in
+    foldl reducer (0,0) (map (doubsTrips) (xs))
+
+main = do
+  handle <- openFile "input.txt" ReadMode
+  contents <- hGetContents handle
+  let (doubs, trips) = countDoubsTrips $ words contents
+
+  print (doubs * trips)
+  hClose handle
