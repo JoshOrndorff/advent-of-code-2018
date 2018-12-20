bd3c8c57fe4c1f7f9e1759643c9ce88a30bc0cf7
Joshy Orndorff <admin@joshorndorff.com>
2018-12-20 11:03:35 -0500

    Solved 18.2

--- day18/js/solution.js
+++ day18/js/solution.js
@@ -8,25 +8,97 @@ let board = fs.readFileSync('../input.txt', 'utf-8')
               .filter(line => (line !== ''))
               .map(line => line.split(''));
 
-// Step through the board the correct number of times
-for (let iterations = 1000000000; iterations > 0; iterations--){
-  board = nextBoard(board);
-  console.log (iterations);
-  //console.log(board);
+// Print both results
+console.log(`Part 1: ${iterate(board, 10)}`);
+console.log(`Part 2: ${solvePart2(board)}`);
+
+
+/**
+ * Iterates the board looking for cycles, and then shortcuts to
+ * calculate the state after a billion iterations
+ * @param board The initial board
+ * @return The resource value at steady state.
+ */
+function solvePart2(board) {
+  // Keep track of the boards I've seen and when I saw them
+  // This may need to be an actual hashmap for efficiency reasons
+  let seen = {};
+  let cycleLength = 0;
+  for (var i = 0; cycleLength === 0; i++) {
+    let flatBoard = flatten(board);
+    if (seen.hasOwnProperty(flatBoard)) {
+      cycleLength = i - seen[flatBoard]
+    }
+    seen[flatBoard] = i;
+    //console.log(seen);
+    board = nextBoard(board);
+  }
+
+  const remainingIterations = (1000000000 - i) % cycleLength;
+  console.log(`remaing iterations: ${remainingIterations}`);
+
+  // Now that we've shortcutted, look return the final answer
+  return iterate(board, remainingIterations);
 }
 
-// Count the cells in the final board
-const flatResult = [].concat(...board);
-const trees = flatResult.filter(c => c === '|').length;
-const lumber = flatResult.filter(c => c === '#').length;
+/**
+ * Flattens a board from [['.', '.'], ['|', '#']] to "..|#"
+ * @param board The original 2D list
+ * @return the final string
+ */
+function flatten(board) {
+  let flat = [].concat(...board);
+  return flat.join();
+}
+
+/**
+ * Calculate the board after a given number of transofrmations
+ * @param board The initial board
+ * @param n The number of iterations
+ * @return The resource value
+ */
+function iterate(board, n) {
+  // Step through the board the correct number of times
+  for (let iterations = n; iterations > 0; iterations--){
+    board = nextBoard(board);
+    //console.log (iterations);
+    //console.log(board);
+  }
+
+  return resourceValue(board);
+}
 
 
-// Print the result
-console.log(`Final resource value is ${trees * lumber}`);
+/**
+ * Calculate the "resource value" (count(trees) * count(lumberyards))
+ * of a given board.
+ * @param board The board to calculate from
+ * @return The resource value
+ */
+function resourceValue(board) {
+  const flatResult = [].concat(...board);
+  const trees = flatResult.filter(c => c === '|').length;
+  const lumber = flatResult.filter(c => c === '#').length;
 
+  return trees * lumber;
+}
 
-
-
+/**
+ * Are two boards equal element-wise? Assume same dimensions.
+ * @param b1 The first board
+ * @param b2 The second board
+ * @return Whether the two boards are the same element-wise
+ */
+function equalBoards(b1, b2) {
+  for (let x = 0; x < b1.length; x++) {
+    for (let y = 0; y < b1[x].length; y++) {
+      if (b1[x][y] !== b2[x][y]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
 
 /**
  * Calculates next board from previous board. Does not mutate previous board.
